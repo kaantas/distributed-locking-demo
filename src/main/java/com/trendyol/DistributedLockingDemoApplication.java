369b1995fc623292bc60359366022a489102f72d
Kaan Taş <kaantas59@gmail.com>
2020-05-16 01:49:17 +0300

    first commit

---
+++ src/main/java/com/trendyol/DistributedLockingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.trendyol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedLockingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedLockingDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trendyol/DistributedLockingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trendyol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedLockingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
